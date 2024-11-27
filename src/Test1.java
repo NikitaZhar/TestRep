f2f2f66fc5ee0c5e1ab8366d9b795e5f48739d46
NikitaZhar <nikita.zharinov@gmail.com>
2024-11-27 13:05:12 +0100

    Next update

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test 1 2 3 4 5 6");
+		System.out.println("Test 1 2 3 4 5 6 7 8 9");
 	}
 
 }
